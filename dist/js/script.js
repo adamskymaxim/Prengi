0cef1dddff32d0cd8c320dce66765cff0ded37f9
Maxim <adamskymaxim@gmail.com>
2022-07-26 19:41:50 +0300

    section industry

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
-    $('.promo__wrapper').slick({
+    $('.promo__wrapp').slick({
       dots: true,
       arrow: false
+    });
+  });
+
+
+  $(document).ready(function(){
+    $('.industry__carousel').slick({
+        speed: 1200,
+        adptiveHeght: false,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-left.png"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-right.png"></img></button>'
     });
   });
       
