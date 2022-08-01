864c9605490242f6b52706c2bc5401db70a83a71
Maxim <adamskymaxim@gmail.com>
2022-08-01 22:16:26 +0300

    i am cheese

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
-    $('.promo__wrapp').slick({
+    $('.promo__slide').slick({
       dots: true,
       arrow: false
     });
   });
 
 
-  $(document).ready(function(){
-    $('.industry__carousel').slick({
-        speed: 1200,
-        adptiveHeght: false,
-        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-left.png"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-right.png"></img></button>'
-    });
+$(document).ready(function(){
+  $('.industry__carousel').slick({
+      speed: 1200,
+      adptiveHeght: false,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/arrow-left.png"></img></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/icons/arrow-right.png"></img></button>',
+      Infinity: true
   });
+});
       
